f77d7f875413f5c0d3c93b35040d68a51243b21b
loppy33 <nikita.petrenko1406@gmail.com>
2024-06-08 19:45:15 +0200

    A

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from 'react';
 import { useExpand, useThemeParams } from '@vkruglikov/react-telegram-web-app';
 
-
+const tg = window.Telegram.WebApp
 
 function App() {
   const [isExpanded, expand] = useExpand();
-  const { tg } = useThemeParams();
 
   useEffect(() => {
     !isExpanded && expand()
-    tg.headerColor("#000000")
+    tg.setHeaderColor("#000000")
   }, [])
 
 

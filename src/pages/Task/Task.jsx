840cedfba4aaec3d87f8bc1c3da14c5efb3a1bce
loppy33 <nikita.petrenko1406@gmail.com>
2024-06-09 12:11:12 +0200

    123

--- src/pages/Task/Task.jsx
+++ src/pages/Task/Task.jsx
@@ -1,12 +1,15 @@
 import './Task.sass'
 import User from "../User";
 import SpaceTask from './components/spacetask/spacetask';
+import { BackButton } from '@vkruglikov/react-telegram-web-app';
 
 export default function Task() {
     return (
         <div className="Task">
             <div className="container">
+                <BackButton onClick={() => console.log('Hello, I am back button!')} />;
                 <User />
+
                 <div className="choice">
                     <button className="tasks active">Space Tasks</button>
                     <button className="achivment">Achivment</button>

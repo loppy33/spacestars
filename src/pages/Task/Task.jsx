7b97e305d13310d5865227be2ddfd19458ce4ac6
loppy33 <nikita.petrenko1406@gmail.com>
2024-06-09 12:24:57 +0200

    change url

--- src/pages/Task/Task.jsx
+++ src/pages/Task/Task.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 export default function Task() {
     let navigate = useNavigate();
     const routeChange = () => {
-        let path = `/`;
+        let path = `/spacestars/`;
         navigate(path);
     }
 

546ef1e5198b42598ff2c89b7e5d9505714ec39d
loppy33 <nikita.petrenko1406@gmail.com>
2024-06-09 12:15:11 +0200

    123

--- src/pages/Task/Task.jsx
+++ src/pages/Task/Task.jsx
@@ -2,12 +2,18 @@ import './Task.sass'
 import User from "../User";
 import SpaceTask from './components/spacetask/spacetask';
 import { BackButton } from '@vkruglikov/react-telegram-web-app';
+import { useNavigate } from "react-router-dom";
 
 export default function Task() {
+    let navigate = useNavigate();
+    const routeChange = () => {
+        let path = `/`;
+        navigate(path);
+    }
     return (
         <div className="Task">
             <div className="container">
-                <BackButton onClick={() => console.log('Hello, I am back button!')} />;
+
                 <User />
 
                 <div className="choice">
@@ -16,6 +22,7 @@ export default function Task() {
                 </div>
                 <SpaceTask />
             </div>
+            <BackButton onClick={() => routeChange} />
         </div>
     )
 }

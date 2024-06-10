f6bd55f48d030b47ee42e3ffb2f03c17aba6a5e5
loppy33 <nikita.petrenko1406@gmail.com>
2024-06-10 15:07:31 +0200

    add achv

--- src/pages/Task/components/achivment/achivment.jsx
+++ src/pages/Task/components/achivment/achivment.jsx
@@ -1,5 +1,10 @@
 import './achivment.sass'
 import FrendPlus from '../../../../assets/icons/frendPlus.png'
+import frendPrem from '../../../../assets/icons/frenPrem.png'
+import Money100 from '../../../../assets/m100.png'
+import Money200 from '../../../../assets/m200.png'
+import Money500 from '../../../../assets/m500.png'
+import Money1000 from '../../../../assets/m1000.png'
 
 export default function Achivment() {
     return (
@@ -14,7 +19,53 @@ export default function Achivment() {
                         </div>
                         <span>20</span>
                     </div>
+                    <ul>
+                        <li className='active'>
+                            <img className='money' src={Money100} alt="" />
+                            <span><img src={FrendPlus} alt="" /> 10</span>
+                        </li>
+                        <li className='active'>
+                            <img className='money' src={Money200} alt="" />
+                            <span><img src={FrendPlus} alt="" /> 20</span>
+                        </li>
+                        <li>
+                            <img className='money' src={Money500} alt="" />
+                            <span><img src={FrendPlus} alt="" /> 50</span>
+                        </li>
+                        <li>
+                            <img className='money' src={Money1000} alt="" />
+                            <span><img src={FrendPlus} alt="" /> 100</span>
+                        </li>
 
+                    </ul>
+                </li>
+                <li>
+                    <div className='infoContainer'>
+                        <div>
+                            <img src={frendPrem} alt="" />
+                            <p>Invite frens</p>
+                        </div>
+                        <span>20</span>
+                    </div>
+                    <ul>
+                        <li>
+                            <img className='money' src={Money100} alt="" />
+                            <span><img src={FrendPlus} alt="" /> 10</span>
+                        </li>
+                        <li>
+                            <img className='money' src={Money200} alt="" />
+                            <span><img src={FrendPlus} alt="" /> 20</span>
+                        </li>
+                        <li>
+                            <img className='money' src={Money500} alt="" />
+                            <span><img src={FrendPlus} alt="" /> 50</span>
+                        </li>
+                        <li>
+                            <img className='money' src={Money1000} alt="" />
+                            <span><img src={FrendPlus} alt="" /> 100</span>
+                        </li>
+
+                    </ul>
                 </li>
             </ul>
 

a1d8837cefbf23418076916c70aae88404febe1c
loppy33 <nikita.petrenko1406@gmail.com>
2024-06-13 22:50:56 +0200

    123

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,16 +4,18 @@ import UserRank from "../assets/rank.png"
 import Border from "../assets/border.png"
 
 export default function User() {
+    // Получение данных пользователя из Telegram WebApp
+    const user = window.Telegram.WebApp.initDataUnsafe.user;
 
     return (
         <div className="User">
             <div className="avatarContainer">
-                <img className='avatar' src={UserAvatr} alt="" />
-                <img className='rank' src={UserRank} alt="" />
-                <img src={Border} className='border' alt="" />
+                <img className='avatar' src={user?.photo_url || UserAvatr} alt={user?.username || 'User Avatar'} />
+                <img className='rank' src={UserRank} alt="Rank" />
+                <img src={Border} className='border' alt="Border" />
             </div>
-            <h3>alexsandr.t</h3>
+            <h3>{user?.username || 'Username'}</h3>
             <h2>50,094.434</h2>
         </div>
-    )
+    );
 }
